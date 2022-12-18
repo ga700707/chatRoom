0c1b1ffaba4103c50e23ee284b33cd2421b927e0
ga700707 <ga700557@gmail.com>
2022-12-18 14:20:41 +0800

    chatSocket server

---
+++ chatSocket/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const { Server } = require("socket.io");
+var cors = require("cors");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+const io = new Server(server, {
+  allowEIO3: true,
+  cors: {
+    origin: "http://localhost:5173",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connection");
+  //   socket.on("send", function (obj) {
+  //     console.log(obj.msg);
+
+  //     socket.broadcast.emit("other", {
+  //       msg: obj.msg,
+  //     });
+  //     socket.emit("self", {
+  //       msg: obj.msg,
+  //     });
+  //   });
+  socket.on("send_Msg", function (obj) {
+    console.log(obj);
+    socket.broadcast.emit("receive_Msg", obj);
+    // socket.emit("receiveMsg", obj);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
