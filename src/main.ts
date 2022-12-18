23eadb51bebd48d3d74e1557b67b87a10ef3aa76
ga700707 <ga700557@gmail.com>
2022-12-18 14:17:40 +0800

    scoketio client

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-
+// import store from './stores'
+import SocketIO from 'socket.io-client'
 import App from "./App.vue";
 import router from "./router";
-
+import VueSocketIO from 'vue-socket.io'
 import "./assets/main.css";
 // import "/dist/output.css"
 import "./style.scss"
@@ -11,7 +12,24 @@ import "./style.scss"
 
 const app = createApp(App);
 
+
+// const options = { path: '/' }; //Options object to pass into SocketIO
+const optionsVueIO = new VueSocketIO({
+    debug: true,
+    connection: SocketIO('http://localhost:3000')
+})
+
+optionsVueIO.install = (function (app: App) {
+    // @ts-ignore
+    app.config.globalProperties.$socket = this.io;
+    // @ts-ignore
+    app.provide('socket', this.io);
+    // @ts-ignore
+    app.config.globalProperties.$vueSocketIo = this
+}).bind(optionsVueIO);
+
+
 app.use(createPinia());
 app.use(router);
-
+app.use(optionsVueIO) 
 app.mount("#app");
